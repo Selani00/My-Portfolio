cf0a4b981be19889aaa7a10d4d8e92e5a7af71fe
Selani00 <pkselani00@gmail.com>
2024-02-17 23:16:46 -0800

    Nav bar changed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>About</div>;
+  return <div className="h-screen">About</div>;
 };
 
 export default Hero;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,122 +1,40 @@
 import React from 'react';
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import useMediaQuery from "../hooks/useMediaQuery";
-import { FaXmark, FaBars } from "react-icons/fa6";
 
 
+const NavBar=()=>{
+    const [sticky,setSticky]=useState(true);
 
-//create custom component
-const Link = ({ page, selectedPage, setSelectedPage }) => {
-    const lowerCasePage = page.toLowerCase();
-    return (
-      <AnchorLink
-        //if selected page is equal to lowerCasePage then text color is yellow otherwise white
-        className={`cursor-pointer ${
-          selectedPage === lowerCasePage ? "text-yellow" : "text-white"
-        } 
-              hover:text-yellow transition duration-500`}
-        //identify where we go when we click the link
-        href={`#${lowerCasePage}`}
-        onClick={() => setSelectedPage(lowerCasePage)}
-      >
-        {page}
-      </AnchorLink>
-    );
-  };
+    const menuLinks =[
+        {name: 'HOME',link:'#home',},
+        {name: 'ABOUT',link:'#about',},
+        {name: 'SKILLS',link:'#skills',},
+        {name: 'PROJECTS',link:'#projects',},
+        {name: 'CONTACT',link:'#contact',}
 
-  const NavBar = ({ isTopOfPage,selectedPage, setSelectedPage }) => {
-    //for mobile device. Check if we click the menu or not
-    const [isMenuToggled, setIsMenuToggled] = useState(false);
-    const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
-    const navbarBackground = isTopOfPage ? "" : "bg-red"; //this will give the different color to the navbar when we scroll down
-  
-    return (
-      <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
-        <div className="flex items-center justify-between mx-auto w-5/6">
-          <h4 className="font-playfair text-3xl font-bold">SD</h4>
-  
-          {/* Desktop Nav */}
-          {isAboveMediumScreens ? (
-            <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Skills"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Projects"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Testimonials"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
+    ]
+    return(
+        <nav className={`fixed w-full left-0 top-0 z-[999] ${sticky ? "md:bg-white text-gray-900":"text-white"}`}>
+            <div className='flex items-center justify-between'>
+                <div className='mx-7'>
+                    <h4 className='text-4xl uppercase font-bold'>
+                        S<span className='text-cyan-600'>D</span>
+                    </h4>
+
+                </div>
+                <div className='text-gray-900 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full'>
+                    <ul className='flex items-center gap-1 py-2 text-lg'>
+                        {
+                            menuLinks?.map((menuLink,index)=>(
+                                <li key={index} className='px-6 hover:text-yellow '>
+                                    <a href={menuLink?.link}>{menuLink?.name}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
-          ) : (
-            <button
-              className="rounded-full bg-red p-2"
-              onClick={() => setIsMenuToggled(!isMenuToggled)}
-            >
-              <FaBars className="h-5 w-5 " />
-            </button>
-          )}
-  
-          {/* Mobile menu pop up */}
-          {!isAboveMediumScreens && isMenuToggled && (
-            <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
-              {/* Close icon */}
-              <div className="flex justify-end p-12">
-                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                  <FaXmark className="h-5 w-5 " />
-                </button>
-              </div>
-  
-              {/* Menu Items */}
-              <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Skills"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Projects"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Testimonials"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </div>
-            </div>
-          )}
-        </div>
-      </nav>
-    );
-  };
-  
-  export default NavBar;
-  
+        </nav>
+        )
+}
+
+export default NavBar;
