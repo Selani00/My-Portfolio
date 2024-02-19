e04051b20a43a0df791a859ed5b956a9ddab7361
Selani00 <pkselani00@gmail.com>
2024-02-19 12:08:06 -0800

    Landing Section added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { FaXmark, FaBars } from "react-icons/fa6";
 
+const NavBar = () => {
+  const [sticky, setSticky] = useState(false);
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
 
-const NavBar=()=>{
-    const [sticky,setSticky]=useState(true);
+  const menuLinks = [
+    { name: "HOME", link: "#home" },
+    { name: "ABOUT", link: "#about" },
+    { name: "SKILLS", link: "#skills" },
+    { name: "PROJECTS", link: "#projects" },
+    { name: "CONTACT", link: "#contact" },
+  ];
 
-    const menuLinks =[
-        {name: 'HOME',link:'#home',},
-        {name: 'ABOUT',link:'#about',},
-        {name: 'SKILLS',link:'#skills',},
-        {name: 'PROJECTS',link:'#projects',},
-        {name: 'CONTACT',link:'#contact',}
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const nav = document.querySelector("nav");
+      window.scrollY > 0 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+  return (
+    <nav
+      className={`fixed w-full left-0 top-0 z-[999] ${
+        sticky ? "bg-white/60  text-gray-900" : "text-white"
+      }`}
+    >
+      <div className="flex items-center justify-between">
+        <div className="mx-7">
+          <h4 className="text-4xl uppercase font-bold ">
+            S<span className="text-cyan-600">D</span>
+          </h4>
+        </div>
+        <div
+          className={` ${
+            sticky ? "md:bg-white/0 bg-white" : "bg-white"
+          } text-gray-900 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full`}
+        >
+          <ul className="flex items-center gap-1 py-2 text-lg">
+            {menuLinks?.map((menuLink, index) => (
+              <li key={index} className="px-6 hover:text-yellow ">
+                <a href={menuLink?.link}>{menuLink?.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-    ]
-    return(
-        <nav className={`fixed w-full left-0 top-0 z-[999] ${sticky ? "md:bg-white text-gray-900":"text-white"}`}>
-            <div className='flex items-center justify-between'>
-                <div className='mx-7'>
-                    <h4 className='text-4xl uppercase font-bold'>
-                        S<span className='text-cyan-600'>D</span>
-                    </h4>
+        <div 
+                onClick={()=>setIsMenuToggled(!isMenuToggled)}
+                className={`z-[999] ${isMenuToggled ? "text-gray-900 " : "text-white"} m-5  text-3xl md:hidden`}>
+                    <FaBars/>
 
-                </div>
-                <div className='text-gray-900 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full'>
-                    <ul className='flex items-center gap-1 py-2 text-lg'>
-                        {
-                            menuLinks?.map((menuLink,index)=>(
-                                <li key={index} className='px-6 hover:text-yellow '>
-                                    <a href={menuLink?.link}>{menuLink?.name}</a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        )
-}
+        </div>
+
+        <div
+          className={`md:hidden text-gray-900 absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 duration-300
+                ${isMenuToggled ? "right-0 " : "right-[-100%] "}`}
+        >
+          <ul className="flex flex-col justify-center h-full gap-10 py-2 text-lg">
+            {menuLinks?.map((menuLink, index) => (
+              <li key={index} className="px-6 hover:text-yellow ">
+                <a href={menuLink?.link}>{menuLink?.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
